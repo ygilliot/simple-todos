888a00b1af236682d180c61a823221b8d4e01c39
Lithium <yann.gilliot@outlook.fr>
2016-01-24 17:13:06 +0100

    Middle check-in

--- simple-todos.js
+++ simple-todos.js
@@ -1,19 +1,58 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    //This code only runs on the client
+    Template.body.helpers({
+        tasks: function () {
+            if (Session.get("hideCompleted")) {
+                //If hide completed is checked, return tasks
+                return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+            } else {
+                //Otherwise, return all of the tasks
+                return Tasks.find({}, { sort: { createdAt: -1 } });
+            }
+        },
+        hideCompleted: function () {
+            return Session.get("hideCompleted");
+        },
+        incompleteCount: function () {
+            return Tasks.find({ checked: { $ne: true } }).count();
+        }
+    });
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Template.body.events({
+        "submit .new-task": function(event){
+            //Prevent default browser form submit
+            event.preventDefault();
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+            //Get value from form element
+            var text = event.target.text.value;
+
+            //Insert a task into the collection
+            Tasks.insert({
+                text: text,
+                createdAt: new Date() //Current time
+            });
+
+            //Clear form
+            event.target.text.value = "";
+        },
+        "change .hide-completed input": function (event) {
+            Session.set("hideCompleted", event.target.checked);
+        }
+    });
+
+    Template.task.events({
+        "click .toggle-checked": function () {
+            //Set the checked property to the opposite of its current value
+            Tasks.update(this._id, {
+                $set: { checked: !this.checked }
+            });
+        },
+        "click .delete": function () {
+            Tasks.remove(this._id);
+        }
+    });
 }
 
 if (Meteor.isServer) {
